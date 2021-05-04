ef072b6eeb68f7325361378cc1cd085975ecc73d
Codding BBQ <codingbbq@gmail.com>
2021-05-04 06:31:29 +0530

    Practice easy problems

--- codeforces-practice/May2021/010_minutes_new_year.cpp
+++ codeforces-practice/May2021/010_minutes_new_year.cpp
@@ -1,10 +1,17 @@
-// 
+// https://codeforces.com/problemset/problem/1283/A
 
 #include <bits/stdc++.h>
 using namespace std;
 
 void solve() {
-    
+    int T;
+    cin >> T;
+    int h, m, total = 0;
+    while(T--) {
+        cin >> h >> m;
+        total = (24*60) - (h*60 + m);
+        cout << total << "\n";
+    }
 }
 
 int main() {
--- codeforces-practice/May2021/011_vanya_cubes.cpp
+++ codeforces-practice/May2021/011_vanya_cubes.cpp
@@ -1,10 +1,23 @@
-// 
+// https://codeforces.com/problemset/problem/492/A
 
 #include <bits/stdc++.h>
 using namespace std;
 
 void solve() {
-    
+    int count;
+    cin >> count;
+    int i = 0, sum = 0;
+    int total = 0;
+    while(true) {
+        sum += i;
+        total+= sum;
+        if(count < total) {
+            i-=1;
+            break;
+        }
+        i++;
+    }
+    cout << i;
 }
 
 int main() {
