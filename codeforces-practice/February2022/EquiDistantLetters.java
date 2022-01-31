0514d49f2862aaecd1596dd83e6a85bd417993a2
codingbbq <codingbbq@gmail.com>
2022-01-31 19:45:59 +0530

    Codeforces easy problem

---
+++ codeforces-practice/February2022/EquiDistantLetters.java
@@ -0,0 +1,54 @@
+// https://codeforces.com/problemset/problem/1626/A
+// A. Equidistant Letters
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class EquiDistantLetters {
+
+    public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+        int t = fs.nextInt();
+        for(int i = 0; i < t; i++) {
+            char[] s = fs.next().toCharArray();
+            Arrays.sort(s);
+            System.out.println(new String(s));
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+        public String next() {
+            while (!st.hasMoreElements())
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a=new int[n];
+            for (int i=0; i<n; i++) a[i]=nextInt();
+            return a;
+        }
+        long[] readLongArray(int n) {
+            long[] a=new long[n];
+            for (int i=0; i<n; i++) a[i]=nextLong();
+            return a;
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+}
