5ab36c37b13a345253dfab07852a0834a4fe0898
codingbbq <codingbbq@gmail.com>
2022-09-23 10:15:51 +0530

    Codeforces Starters57 problems

---
+++ codechef-practice/September2022/Starters57EvenSplits.java
@@ -0,0 +1,122 @@
+// https://www.codechef.com/START57D/problems/EVENSPLIT
+// Even Splits
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+class Starters57EvenSplits {
+
+    void solve(int TC) throws Exception {
+        int n = ni();
+        String b = ns();
+        String out = "";
+        if(b.length() <= 2) {
+            out += b;
+        } else {
+            int zero = 0;
+            String outZ = "";
+            String outO = "";
+            for(int x = 0; x < n; x++) {
+                if(b.charAt(x) == '0') {
+                    zero++;
+                    outZ += "0";
+                } else {
+                    outO += "1";
+                }
+            }
+            out += outZ + outO;
+        }
+        println(out);
+    }
+
+    long mod = (long)998244353, IINF = (long)1e17;
+    final int MAX = (int)1e3+1, INF = (int)2e9, root = 3;
+    DecimalFormat df = new DecimalFormat("0.0000000000000");
+    double PI = 3.1415926535897932384626433832792884197169399375105820974944, eps = 1e-8;
+
+    static boolean multipleTC = true, memory = false;
+
+    FastReader in;
+    PrintWriter out;
+
+    void run() throws Exception{
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        int T = (multipleTC) ? ni():1;
+        for(int i = 1; i<= T; i++) {
+            solve(i);
+        }
+        out.flush();
+        out.close();
+    }
+    public static void main(String[] args) throws Exception{
+        if(memory) new Thread(null, new Runnable() {public void run(){try{new Starters57EvenSplits().run();}catch(Exception e){e.printStackTrace();}}}, "1", 1 << 28).start();
+        else new Starters57EvenSplits().run();
+    }
+
+    long gcd(long a, long b){ return (b==0)?a:gcd(b,a%b); }
+    int gcd(int a, int b){return (b==0)?a:gcd(b,a%b);}
+    int bit(long n){return (n==0)?0:(1+bit(n&(n-1)));}
+
+    // Find factorial using recursion
+    long factorial(long n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
+    // Print
+    void print(Object o){out.print(o);}
+
+    // Print next line
+    void println(Object o){out.println(o);}
+
+    void pni(Object o){out.println(o);out.flush();}
+
+    // String next
+    String ns(){return in.next();}
+    // String new line
+    String nsln(){return in.nextLine();}
+
+    // Integer
+    int ni(){return Integer.parseInt(in.next());}
+    // Long
+    long nl(){return Long.parseLong(in.next());}
+    // Double
+    double nd(){return Double.parseDouble(in.next());}
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws Exception{
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException  e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
