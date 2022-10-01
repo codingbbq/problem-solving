c7f4d81cf564a499d2b0e8c210edf635eb63703f
codingbbq <codingbbq@gmail.com>
2022-10-01 22:45:58 +0530

    Codeforces practice problems

---
+++ codeforces-practice/September2022/Dictionary.java
@@ -0,0 +1,121 @@
+// https://codeforces.com/problemset/problem/1674/B
+// B. Dictionary
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class Dictionary {
+
+    // Generate your map here.
+    Map<String, Integer> generateMap() {
+        Map<String, Integer> map = new HashMap<>();
+        int index = 0;
+        for(int i = 0; i < 26; i++) {
+            for(int j = 0; j < 26; j++) {
+                if(i != j) {
+                    String x = Character.toString((char)(i + 97)) + "" + Character.toString((char)(j + 97));
+                    map.put(x, ++index);
+                }
+            }
+        }
+        return map;
+    }
+
+
+    void solve(int TC, Map<String, Integer> m) throws Exception {
+        String s = ns();
+        println(m.get(s));
+    }
+
+    long mod = (long)998244353, IINF = (long)1e17;
+    final int MAX = (int)1e3+1, INF = (int)2e9, root = 3;
+    DecimalFormat df = new DecimalFormat("0.0000000000000");
+    double PI = 3.1415926535897932384626433832792884197169399375105820974944, eps = 1e-8;
+
+    static boolean multipleTC = true, memory = false;
+
+    FastReader in;
+    PrintWriter out;
+
+    void run() throws Exception{
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        int T = (multipleTC) ? ni():1;
+        Map<String, Integer> myMap = generateMap();
+        for(int i = 1; i<= T; i++) {
+            solve(i, myMap);
+        }
+        out.flush();
+        out.close();
+    }
+    public static void main(String[] args) throws Exception{
+        if(memory) new Thread(null, new Runnable() {public void run(){try{new Dictionary().run();}catch(Exception e){e.printStackTrace();}}}, "1", 1 << 28).start();
+        else new Dictionary().run();
+    }
+
+    long gcd(long a, long b){ return (b==0)?a:gcd(b,a%b); }
+    int gcd(int a, int b){return (b==0)?a:gcd(b,a%b);}
+    int bit(long n){return (n==0)?0:(1+bit(n&(n-1)));}
+
+    // Find factorial using recursion
+    long factorial(long n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
+    // Print
+    void print(Object o){out.print(o);}
+
+    // Print next line
+    void println(Object o){out.println(o);}
+
+    void pni(Object o){out.println(o);out.flush();}
+
+    // String next
+    String ns(){return in.next();}
+    // String new line
+    String nsln(){return in.nextLine();}
+
+    // Integer
+    int ni(){return Integer.parseInt(in.next());}
+    // Long
+    long nl(){return Long.parseLong(in.next());}
+    // Double
+    double nd(){return Double.parseDouble(in.next());}
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws Exception{
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException  e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
