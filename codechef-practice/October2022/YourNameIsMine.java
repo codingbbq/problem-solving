22fe8a1a82123771e619bc5b815341a212dd2877
codingbbq <codingbbq@gmail.com>
2022-10-21 16:59:51 +0530

    CodeChef practice problems

---
+++ codechef-practice/October2022/YourNameIsMine.java
@@ -0,0 +1,126 @@
+// https://www.codechef.com/submit/NAME2
+// Your Name is Mine
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+class YourNameIsMine {
+
+    void solve(int TC) throws Exception {
+        String S1 = ns(), S2 = ns();
+        String X = (S1.length() > S2.length()) ? S2 : S1; // smaller
+        String Y = (S1.length() > S2.length()) ? S1 : S2; // larger
+
+        int A = X.length();
+        int B = Y.length(); // larger
+
+        int k = 0; // Keeps track of the matched character
+        for(int i = 0; i < B; i++) {
+            if(X.charAt(k) == Y.charAt(i)) {
+                k++;
+            }
+            if(k == A){
+                break;
+            }
+        }
+
+        // Checked the matched count with the smallest string length
+        if(k == A) {
+            println("YES");
+        } else {
+            println("NO");
+        }
+
+    }
+
+    long mod = (long)998244353, IINF = (long)1e17;
+    final int MAX = (int)1e3+1, INF = (int)2e9, root = 3;
+    DecimalFormat df = new DecimalFormat("0.0000000000000");
+    double PI = 3.1415926535897932384626433832792884197169399375105820974944, eps = 1e-8;
+
+    static boolean multipleTC = true, memory = false;
+
+    FastReader in;
+    PrintWriter out;
+
+    void run() throws Exception{
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        int T = (multipleTC) ? ni():1;
+        for(int i = 1; i<= T; i++) {
+            solve(i);
+        }
+        out.flush();
+        out.close();
+    }
+    public static void main(String[] args) throws Exception{
+        if(memory) new Thread(null, new Runnable() {public void run(){try{new YourNameIsMine().run();}catch(Exception e){e.printStackTrace();}}}, "1", 1 << 28).start();
+        else new YourNameIsMine().run();
+    }
+
+    long gcd(long a, long b){ return (b==0)?a:gcd(b,a%b); }
+    int gcd(int a, int b){return (b==0)?a:gcd(b,a%b);}
+    int bit(long n){return (n==0)?0:(1+bit(n&(n-1)));}
+
+    // Find factorial using recursion
+    long factorial(long n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
+    // Print
+    void print(Object o){out.print(o);}
+
+    // Print next line
+    void println(Object o){out.println(o);}
+
+    void pni(Object o){out.println(o);out.flush();}
+
+    // String next
+    String ns(){return in.next();}
+    // String new line
+    String nsln(){return in.nextLine();}
+
+    // Integer
+    int ni(){return Integer.parseInt(in.next());}
+    // Long
+    long nl(){return Long.parseLong(in.next());}
+    // Double
+    double nd(){return Double.parseDouble(in.next());}
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws Exception{
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException  e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
