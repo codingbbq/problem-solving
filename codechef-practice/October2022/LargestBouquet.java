e0b939c2e5499d8486e031309e5f56ac7ed56063
codingbbq <codingbbq@gmail.com>
2022-10-24 23:33:57 +0530

    CodeChef practice problems

---
+++ codechef-practice/October2022/LargestBouquet.java
@@ -0,0 +1,128 @@
+// https://www.codechef.com/submit/BOUQUET
+// The Largest Bouquet
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+class LargestBouquet {
+
+    void solve(int TC) throws Exception {
+        long[][] mat = new long[3][3];
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                mat[i][j] = ni();
+            }
+        }
+
+        long max = 0;
+        for(int i = 0; i < 3; i++) {
+            long type = 0;
+            long color = 0;
+            for(int j = 0; j < 3; j++) {
+                type += mat[i][j];
+                color += mat[j][i];
+            }
+            if(type % 2 == 0) {
+                type -= 1;
+            }
+            max = Math.max(max, type);
+
+            if(color % 2 == 0) {
+                color -= 1;
+            }
+            max = Math.max(max, color);
+        }
+        println(max);
+    }
+
+    long mod = (long)998244353, IINF = (long)1e17;
+    final int MAX = (int)1e3+1, INF = (int)2e9, root = 3;
+    DecimalFormat df = new DecimalFormat("0.0000000000000");
+    double PI = 3.1415926535897932384626433832792884197169399375105820974944, eps = 1e-8;
+
+    static boolean multipleTC = true, memory = false;
+
+    FastReader in;
+    PrintWriter out;
+
+    void run() throws Exception{
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        int T = (multipleTC) ? ni():1;
+        for(int i = 1; i<= T; i++) {
+            solve(i);
+        }
+        out.flush();
+        out.close();
+    }
+    public static void main(String[] args) throws Exception{
+        if(memory) new Thread(null, new Runnable() {public void run(){try{new LargestBouquet().run();}catch(Exception e){e.printStackTrace();}}}, "1", 1 << 28).start();
+        else new LargestBouquet().run();
+    }
+
+    long gcd(long a, long b){ return (b==0)?a:gcd(b,a%b); }
+    int gcd(int a, int b){return (b==0)?a:gcd(b,a%b);}
+    int bit(long n){return (n==0)?0:(1+bit(n&(n-1)));}
+
+    // Find factorial using recursion
+    long factorial(long n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
+    // Print
+    void print(Object o){out.print(o);}
+
+    // Print next line
+    void println(Object o){out.println(o);}
+
+    void pni(Object o){out.println(o);out.flush();}
+
+    // String next
+    String ns(){return in.next();}
+    // String new line
+    String nsln(){return in.nextLine();}
+
+    // Integer
+    int ni(){return Integer.parseInt(in.next());}
+    // Long
+    long nl(){return Long.parseLong(in.next());}
+    // Double
+    double nd(){return Double.parseDouble(in.next());}
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws Exception{
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException  e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
