7aba655ad694ff902db4dd4d0b179ecc7148088b
codingbbq <codingbbq@gmail.com>
2022-10-26 09:31:36 +0530

    CodeChef practice problems

---
+++ codechef-practice/October2022/GuessingGame.java
@@ -0,0 +1,113 @@
+// https://www.codechef.com/submit/GUESS
+// Guessing Game
+
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+class GuessingGame {
+
+    void solve(int TC) throws Exception {
+        long N = ni(), M = ni();
+        long totalcombinations = N * M;
+        long NEven = N/2;
+        long MEven = M/2;
+        long oddCombi = Math.abs(((MEven * NEven) + ((N - NEven) * (M - MEven))) - totalcombinations);
+        long g = gcd(oddCombi, totalcombinations);
+        print(g > 1 && oddCombi > 0 ? oddCombi/g : oddCombi);
+        print("/");
+        print(g > 1 ? totalcombinations/g : totalcombinations);
+        println("");
+    }
+
+    long mod = (long)998244353, IINF = (long)1e17;
+    final int MAX = (int)1e3+1, INF = (int)2e9, root = 3;
+    DecimalFormat df = new DecimalFormat("0.0000000000000");
+    double PI = 3.1415926535897932384626433832792884197169399375105820974944, eps = 1e-8;
+
+    static boolean multipleTC = true, memory = false;
+
+    FastReader in;
+    PrintWriter out;
+
+    void run() throws Exception{
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        int T = (multipleTC) ? ni():1;
+        for(int i = 1; i<= T; i++) {
+            solve(i);
+        }
+        out.flush();
+        out.close();
+    }
+    public static void main(String[] args) throws Exception{
+        if(memory) new Thread(null, new Runnable() {public void run(){try{new GuessingGame().run();}catch(Exception e){e.printStackTrace();}}}, "1", 1 << 28).start();
+        else new GuessingGame().run();
+    }
+
+    long gcd(long a, long b){ return (b==0)?a:gcd(b,a%b); }
+    int gcd(int a, int b){return (b==0)?a:gcd(b,a%b);}
+    int bit(long n){return (n==0)?0:(1+bit(n&(n-1)));}
+
+    // Find factorial using recursion
+    long factorial(long n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
+    // Print
+    void print(Object o){out.print(o);}
+
+    // Print next line
+    void println(Object o){out.println(o);}
+
+    void pni(Object o){out.println(o);out.flush();}
+
+    // String next
+    String ns(){return in.next();}
+    // String new line
+    String nsln(){return in.nextLine();}
+
+    // Integer
+    int ni(){return Integer.parseInt(in.next());}
+    // Long
+    long nl(){return Long.parseLong(in.next());}
+    // Double
+    double nd(){return Double.parseDouble(in.next());}
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws Exception{
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException  e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
---
+++ codechef-practice/October2022/PlusleAndMinunArray.java
@@ -0,0 +1,140 @@
+// https://www.codechef.com/submit/PMA?tab=statement
+// Plusle and Minun on Array
+
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+class PlusleAndMinunArray  {
+
+    void solve(int TC) throws Exception {
+        int N = ni();
+        long[] A = new long[N];
+        for(int i = 0; i < N; i++) {
+            long x = Math.abs(nl());
+            A[i] = x;
+        }
+
+        int minPtr = 0;
+        int maxPtr = 1;
+        for(int i = 0; i < N; i++) {
+            if(i % 2 == 0) {
+                if(A[minPtr] > A[i]) {
+                    minPtr = i;
+                }
+            } else {
+                if(A[maxPtr] < A[i]) {
+                    maxPtr = i;
+                }
+            }
+        }
+
+        // Swap the numbers;
+        if(A[minPtr] < A[maxPtr]) {
+            long temp = A[minPtr];
+            A[minPtr] = A[maxPtr];
+            A[maxPtr] = temp;
+        }
+
+        long sum = 0;
+        for(int i = 0; i < N; i++) {
+            if(i % 2 == 0) {
+                sum += A[i];
+            } else {
+                sum -= A[i];
+            }
+        }
+        println(sum);
+    }
+
+    long mod = (long)998244353, IINF = (long)1e17;
+    final int MAX = (int)1e3+1, INF = (int)2e9, root = 3;
+    DecimalFormat df = new DecimalFormat("0.0000000000000");
+    double PI = 3.1415926535897932384626433832792884197169399375105820974944, eps = 1e-8;
+
+    static boolean multipleTC = true, memory = false;
+
+    FastReader in;
+    PrintWriter out;
+
+    void run() throws Exception{
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        int T = (multipleTC) ? ni():1;
+        for(int i = 1; i<= T; i++) {
+            solve(i);
+        }
+        out.flush();
+        out.close();
+    }
+    public static void main(String[] args) throws Exception{
+        if(memory) new Thread(null, new Runnable() {public void run(){try{new PlusleAndMinunArray ().run();}catch(Exception e){e.printStackTrace();}}}, "1", 1 << 28).start();
+        else new PlusleAndMinunArray ().run();
+    }
+
+    long gcd(long a, long b){ return (b==0)?a:gcd(b,a%b); }
+    int gcd(int a, int b){return (b==0)?a:gcd(b,a%b);}
+    int bit(long n){return (n==0)?0:(1+bit(n&(n-1)));}
+
+    // Find factorial using recursion
+    long factorial(long n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
+    // Print
+    void print(Object o){out.print(o);}
+
+    // Print next line
+    void println(Object o){out.println(o);}
+
+    void pni(Object o){out.println(o);out.flush();}
+
+    // String next
+    String ns(){return in.next();}
+    // String new line
+    String nsln(){return in.nextLine();}
+
+    // Integer
+    int ni(){return Integer.parseInt(in.next());}
+    // Long
+    long nl(){return Long.parseLong(in.next());}
+    // Double
+    double nd(){return Double.parseDouble(in.next());}
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws Exception{
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException  e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
