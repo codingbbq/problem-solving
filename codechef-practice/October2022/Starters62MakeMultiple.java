d31c63ff91ddafc26f88bca8bf12452e672b5ebe
codingbbq <codingbbq@gmail.com>
2022-10-27 16:01:17 +0530

    CodeChef practice problems

---
+++ codechef-practice/October2022/Starters62MakeMultiple.java
@@ -0,0 +1,108 @@
+// https://www.codechef.com/START62C/problems/MAKEMULTIPLE
+// Make Multiple
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+class Starters62MakeMultiple  {
+
+    void solve(int TC) throws Exception {
+        long A = nl(), B = nl();
+        if(A==B || 2*A <= B) {
+            println("YES");
+        } else {
+            println("NO");
+        }
+    }
+
+    long mod = (long)998244353, IINF = (long)1e17;
+    final int MAX = (int)1e3+1, INF = (int)2e9, root = 3;
+    DecimalFormat df = new DecimalFormat("0.0000000000000");
+    double PI = 3.1415926535897932384626433832792884197169399375105820974944, eps = 1e-8;
+
+    static boolean multipleTC = true, memory = false;
+
+    FastReader in;
+    PrintWriter out;
+
+    void run() throws Exception{
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        int T = (multipleTC) ? ni():1;
+        for(int i = 1; i<= T; i++) {
+            solve(i);
+        }
+        out.flush();
+        out.close();
+    }
+    public static void main(String[] args) throws Exception{
+        if(memory) new Thread(null, new Runnable() {public void run(){try{new Starters62MakeMultiple().run();}catch(Exception e){e.printStackTrace();}}}, "1", 1 << 28).start();
+        else new Starters62MakeMultiple().run();
+    }
+
+    long gcd(long a, long b){ return (b==0)?a:gcd(b,a%b); }
+    int gcd(int a, int b){return (b==0)?a:gcd(b,a%b);}
+    int bit(long n){return (n==0)?0:(1+bit(n&(n-1)));}
+
+    // Find factorial using recursion
+    long factorial(long n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
+    // Print
+    void print(Object o){out.print(o);}
+
+    // Print next line
+    void println(Object o){out.println(o);}
+
+    void pni(Object o){out.println(o);out.flush();}
+
+    // String next
+    String ns(){return in.next();}
+    // String new line
+    String nsln(){return in.nextLine();}
+
+    // Integer
+    int ni(){return Integer.parseInt(in.next());}
+    // Long
+    long nl(){return Long.parseLong(in.next());}
+    // Double
+    double nd(){return Double.parseDouble(in.next());}
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws Exception{
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException  e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
---
+++ codechef-practice/October2022/Starters62SortedSubstring.java
@@ -0,0 +1,136 @@
+// https://www.codechef.com/START62C/problems/SSUBSTR
+// Sorted Substrings
+
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+class Starters62SortedSubstring  {
+
+    void solve(int TC) throws Exception {
+        int N = ni();
+        String s = ns();
+
+        // If number start with 0, remove all the one
+        // If number start with 1, remove all the zero
+        boolean removezero = false;
+        if(s.charAt(0) == '1') {
+            removezero = true;
+        }
+
+        int count = removeElements(removezero, s);
+
+        // if we want to remove 1 but the last element is 1, then we need to reduce one operation.
+        if(!removezero && s.charAt(s.length() - 1) == '1') {
+            count -= 1;
+        }
+        println(count);
+
+    }
+
+    int removeElements(boolean r, String s) {
+        boolean p = true;
+        int pair = 0;
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == (r ? '0' : '1')) {
+                if(p) {
+                    pair++;
+                }
+                p = false;
+            } else {
+                p = true;
+            }
+        }
+        return pair;
+    }
+
+    long mod = (long)998244353, IINF = (long)1e17;
+    final int MAX = (int)1e3+1, INF = (int)2e9, root = 3;
+    DecimalFormat df = new DecimalFormat("0.0000000000000");
+    double PI = 3.1415926535897932384626433832792884197169399375105820974944, eps = 1e-8;
+
+    static boolean multipleTC = true, memory = false;
+
+    FastReader in;
+    PrintWriter out;
+
+    void run() throws Exception{
+        in = new FastReader();
+        out = new PrintWriter(System.out);
+        int T = (multipleTC) ? ni():1;
+        for(int i = 1; i<= T; i++) {
+            solve(i);
+        }
+        out.flush();
+        out.close();
+    }
+    public static void main(String[] args) throws Exception{
+        if(memory) new Thread(null, new Runnable() {public void run(){try{new Starters62SortedSubstring().run();}catch(Exception e){e.printStackTrace();}}}, "1", 1 << 28).start();
+        else new Starters62SortedSubstring().run();
+    }
+
+    long gcd(long a, long b){ return (b==0)?a:gcd(b,a%b); }
+    int gcd(int a, int b){return (b==0)?a:gcd(b,a%b);}
+    int bit(long n){return (n==0)?0:(1+bit(n&(n-1)));}
+
+    // Find factorial using recursion
+    long factorial(long n){
+        if (n == 0)
+            return 1;
+        else
+            return(n * factorial(n-1));
+    }
+
+    // Print
+    void print(Object o){out.print(o);}
+
+    // Print next line
+    void println(Object o){out.println(o);}
+
+    void pni(Object o){out.println(o);out.flush();}
+
+    // String next
+    String ns(){return in.next();}
+    // String new line
+    String nsln(){return in.nextLine();}
+
+    // Integer
+    int ni(){return Integer.parseInt(in.next());}
+    // Long
+    long nl(){return Long.parseLong(in.next());}
+    // Double
+    double nd(){return Double.parseDouble(in.next());}
+
+    class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws Exception{
+            br = new BufferedReader(new FileReader(s));
+        }
+
+        String next(){
+            while (st == null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch (IOException  e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
